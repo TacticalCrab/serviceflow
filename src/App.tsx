b60eb0ff1a5f128c426755fd9ce790f91f3af42a
Piotr <pkaleksandrowicz@gmail.com>
2026-08-24 21:05:45 +0200

    minor printing fix

--- src/App.tsx
+++ src/App.tsx
@@ -21,12 +21,18 @@ function AppLayout() {
   useAppFontSize()
   const location = useLocation()
   const isRepairsList = location.pathname === "/naprawy"
+  const isRepairCard =
+    location.pathname === "/karta-naprawy" ||
+    /^\/naprawy\/\d+\/karta-naprawy$/.test(location.pathname)
 
   return (
-    <div className="min-h-screen bg-background print:bg-white">
+    <div
+      className={`min-h-screen bg-background print:bg-white ${isRepairCard ? "print:contents" : ""}`}
+      data-repair-card-layout={isRepairCard || undefined}
+    >
       <Navbar />
       <main
-        className={`mx-auto w-full ${isRepairsList ? "max-w-[1800px]" : "max-w-7xl"} px-4 py-6 sm:px-6 lg:px-8 lg:py-8 print:max-w-none print:p-0`}
+        className={`mx-auto w-full ${isRepairsList ? "max-w-[1800px]" : "max-w-7xl"} px-4 py-6 sm:px-6 lg:px-8 lg:py-8 print:max-w-none print:p-0 ${isRepairCard ? "print:contents" : ""}`}
       >
         <Outlet />
       </main>
